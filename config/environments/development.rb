30358cf8333efd455bab9544ba310c41be24ccd1
suzuki tetsuo <vulture0902@gmail.com>
2017-04-09 03:27:05 +0000

    tmp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'example.com',
+    :authentication => 'plain',
+    :user_name => '自分のユーザー名(gmailのアドレス)',
+    :password => '自分のパスワード'
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
